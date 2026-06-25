c60b797645f81b03bfe5501ec032c5ac2c1953fb
Yuvraj Singh Chouhan <162985002+yuvrajsinghchouhan001@users.noreply.github.com>
2026-06-25 15:24:12 +0530

    index.js

--- init/index.js
+++ init/index.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 const initData  = require("./data.js");
 const Listing = require("../models/listing.js");
 
-const MONGO_URL = "mongodb://127.0.0.1:27017/wanderlust";
+const MONGO_URL = "mongodb://127.0.0.1:27017/HouseHunt";
 main()
     .then(()=>{
         console.log("connected to DB");
@@ -17,6 +17,7 @@ async function main() {
 
 const initDB = async () => {
     await Listing.deleteMany({});
+    initData.data = initData.data.map((obj) => ({...obj , owner:"6a390062e263ff74dbe5e860"}));
     await Listing.insertMany(initData.data);
     console.log("data was initialized");
 
